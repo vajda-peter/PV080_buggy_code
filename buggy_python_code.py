f0817f833cece8bc2927502e0f41b1f4b7d42577
Peter Vajda <petervajda@mail.muni.cz>
2025-04-28 19:03:31 +0200

    fixed some CodaQL issue (hopefully again)

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -28,6 +28,9 @@ def print_nametag(format_string, person):
 def fetch_website(urllib_version, url):
     """Fetches the website."""
     # Import the requested version (2 or 3) of urllib
+    version_patter = "(?:3)?\s+(\d+\.\d+\.\d+)"
+    if not re.match(urlib_version, version_patter):
+        raise ValueError(urlib_version)
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
 
@@ -61,9 +64,6 @@ if __name__ == '__main__':
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
-        version_patter = "(?:3)?\s+(\d+\.\d+\.\d+)"
-        if not re.match(urlib_version, version_patter):
-            raise ValueError(urlib_version)
         fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
         load_yaml(input("File name: "))
